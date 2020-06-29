cf843dcb5e8a666e24b9dd9c2087df9749bc6802
Ji_Rath <garethaguilar@gmail.com>
2020-06-29 14:06:18 -0500

    Relocate files and add initial files

---
+++ Source/EscapeProject/EscapeProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EscapeProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESCAPEPROJECT_API AEscapeProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
